bb8eeb00823daff35df9d905266dade3bb336dbf
Sean Kell <sean.michael.kell@gmail.com>
2015-12-23 08:45:17 -0500

    Began working on implementing memory paging.
    Using walthrough here: http://os.phil-opp.com/modifying-page-tables.html

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,5 +1,7 @@
+use self::paging::PhysicalAddress;
 pub use self::area_frame_allocator::AreaFrameAllocator;
 
+mod paging;
 mod area_frame_allocator;
 
 pub const PAGE_SIZE: usize = 4096;
@@ -12,6 +14,10 @@ pub struct Frame {
 impl Frame {
 	fn containing_address(address: usize) -> Frame {
 		Frame{ number: address / PAGE_SIZE }
+	}
+
+	fn start_address(&self) -> PhysicalAddress {
+		self.number * PAGE_SIZE
 	}
 }
 
---
+++ src/memory/paging/mod.rs
@@ -0,0 +1,63 @@
+use memory::PAGE_SIZE;
+use memory::Frame;
+
+mod entry;
+mod table;
+
+const ENTRY_COUNT: usize = 512;
+
+pub type PhysicalAddress = usize;
+pub type VirtualAddress = usize;
+
+pub struct Page {
+	number: usize,
+}
+
+impl Page {
+	fn containing_address(address: VirtualAddress) -> Page {
+		assert!(address < 0x0000_8000_0000_0000 || address >= 0xffff_8000_0000_0000,
+			"invalid address: 0x{:x}", address);
+		Page { number: address / PAGE_SIZE }		
+	}
+
+	fn start_address(&self) -> usize {
+		self.number * PAGE_SIZE
+	}
+
+	fn p4_index(&self) -> usize {
+		(self.number >> 27) & 0o777
+	}
+
+	fn p3_index(&self) -> usize {
+		(self.number >> 18) & 0o777
+	}
+
+	fn p2_index(&self) -> usize {
+		(self.number >> 9) & 0o777
+	}
+
+	fn p1_index(&self) -> usize {
+		(self.number >> 0) & 0o777
+	}
+}
+
+pub fn translate(virtual_address: VirtualAddress) -> Option<PhysicalAddress> {
+	let offset = virtual_address % PAGE_SIZE;
+	translate_page(Page::containing_address(virtual_address))
+		.map(|frame| frame.number * PAGE_SIZE + offset)
+}
+
+fn translate_page(page: Page) -> Option<Frame> {
+	use self::entry::HUGE_PAGE;
+
+	let p3 = unsafe { &*table::P4 }.next_table(page.p4_index());
+
+	let huge_page = || {
+		// TODO
+	};
+
+	p3.and_then(|p3| p3.next_table(page.p3_index()))
+	  .and_then(|p2| p2.next_table(page.p2_index()))
+	  .and_then(|p1| p1[page.p1_index()].pointed_frame())
+	  .or_else(huge_page)
+}
