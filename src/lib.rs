940331a1b27028eb43c9d3a05e32164c37de75ba
Sean Kell <sean.michael.kell@gmail.com>
2015-12-22 20:51:15 -0500

    Implement VGA interface with formatting macros!

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,17 @@
-#![feature(lang_items)]
+#![feature(lang_items, const_fn, unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-	// ATTENTION: we have a very small stack and no guard page
-
-	let hello = b"Hello world!";
-	let color_byte = 0x1f;	// white foreground, blue background 
-	let mut hello_colored = [color_byte; 24];
-	for (i, char_byte) in hello.into_iter().enumerate() {
-		hello_colored[i*2] = *char_byte;
-	}
-
-	// write `Hello World!` to the center of the VGA text buffer 
-	let buffer_ptr = (0xb8000 + 1988) as *mut _;
-	unsafe { *buffer_ptr = hello_colored };
+	// ATTENTION: we have a very small stack and no guard page 
+	vga_buffer::clear_screen();
+	println!("Hello World{}", "!");
 
 	loop {}
 }
